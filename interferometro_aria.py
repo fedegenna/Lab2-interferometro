aa6518d1834fe5a512e071d20fa19eefd249d78a
fedegenna <federicogenna14@gmail.com>
2025-03-28 15:21:18 +0100

    Update interferometro_aria.py

--- interferometro_aria.py
+++ interferometro_aria.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import chi2
 from mylib import stats
 
-def grafico_deltaN(P_i, P_f, deltaN, d, lambda_, errori_deltaN, errori_P_i, errori_P_f, titolo="Indice di rifrazione dell'aria", xlabel="(P_i - P_f)", ylabel="DeltaN"):
+def grafico_deltaN(delta_P, deltaN, d, lambda_, errori_deltaN, errori_delta_P, titolo="Indice di rifrazione dell'aria", xlabel="delta_P", ylabel="DeltaN"):
     """
     Disegna un grafico con DeltaN in ordinata e (P_i - P_f) in ascissa.
     Calcola il valore di m con il suo errore, chi-quadro e p-value, e li stampa sul grafico.
@@ -21,12 +21,12 @@ def grafico_deltaN(P_i, P_f, deltaN, d, lambda_, errori_deltaN, errori_P_i, erro
     :param ylabel: Etichetta dell'asse y (opzionale).
     """
     # Calcola (P_i - P_f) e i relativi errori
-    x = np.array(P_i) - np.array(P_f)
-    errore_x = np.sqrt(np.array(errori_P_i)**2 + np.array(errori_P_f)**2)
+    x = np.array(delta_P)
+    errore_x = np.ones(len(x))
 
     # DeltaN e i relativi errori
     y = np.array(deltaN)
-    errore_y = np.array(errori_deltaN)
+    errore_y = np.array(errori_deltaP)
 
     # Normalizza x e y per includere d e lambda_
     x_normalizzato = x * 2 * d / lambda_
@@ -71,65 +71,16 @@ def grafico_deltaN(P_i, P_f, deltaN, d, lambda_, errori_deltaN, errori_P_i, erro
 
 def main():
     # Dati
-    P_i = []  # Pressione iniziale
-    P_f = []  # Pressione finale
-    P_i_1 = [0.4, 0.6, 0.7, 0.8, 0.9, 1.0]  # Pressione iniziale
-    P_i.append(stats(P_i_1).mean())
-    P_i_2 = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]  # Pressione iniziale
-    P_i.append(stats(P_i_2).mean())
-    P_i_3 = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]  # Pressione iniziale
-    P_i.append(stats(P_i_3).mean())
-    P_i_4 = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]  # Pressione iniziale
-    P_i.append(stats(P_i_4).mean())
-    P_i_5 = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]  # Pressione iniziale
-    P_i.append(stats(P_i_5).mean())
-    P_f_1 = [0.6, 0.6, 0.7, 0.8, 0.9, 1.0]  # Pressione finale
-    P_f.append(stats(P_f_1).mean())
-    P_f_2 = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]  # Pressione finale
-    P_f.append(stats(P_f_2).mean())
-    P_f_3 = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]  # Pressione finale
-    P_f.append(stats(P_f_3).mean())
-    P_f_4 = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]  # Pressione finale
-    P_f.append(stats(P_f_4).mean())
-    P_f_5 = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]  # Pressione finale
-    P_f.append(stats(P_f_5).mean())
+    delta_P = [5,8.5,11.5,14,18]
+    deltaN = [4,6,7,9,11]  # DeltaN
+    
 
-    errori_P_i = []  # Errori su P_i
-    errori_P_i.append(stats(P_i_1).sigma_mean())
-    errori_P_i.append(stats(P_i_2).sigma_mean())
-    errori_P_i.append(stats(P_i_3).sigma_mean())
-    errori_P_i.append(stats(P_i_4).sigma_mean())
-    errori_P_i.append(stats(P_i_5).sigma_mean())
-    errori_P_f = []  # Errori su P_f
-    errori_P_f.append(stats(P_f_1).sigma_mean())
-    errori_P_f.append(stats(P_f_2).sigma_mean())
-    errori_P_f.append(stats(P_f_3).sigma_mean())
-    errori_P_f.append(stats(P_f_4).sigma_mean())
-    errori_P_f.append(stats(P_f_5).sigma_mean())
-
-    deltaN = []  # DeltaN
-    deltaN_1 = [0.7, 0.6, 0.7, 0.8, 0.9, 1.0]
-    deltaN.append(stats(deltaN_1).mean())
-    deltaN_2 = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    deltaN.append(stats(deltaN_2).mean())
-    deltaN_3 = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    deltaN.append(stats(deltaN_3).mean())
-    deltaN_4 = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    deltaN.append(stats(deltaN_4).mean())
-    deltaN_5 = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    deltaN.append(stats(deltaN_5).mean())
-
-    errori_deltaN = []  # Errori su DeltaN
-    errori_deltaN.append(stats(deltaN_1).sigma_mean())
-    errori_deltaN.append(stats(deltaN_2).sigma_mean())
-    errori_deltaN.append(stats(deltaN_3).sigma_mean())
-    errori_deltaN.append(stats(deltaN_4).sigma_mean())
-    errori_deltaN.append(stats(deltaN_5).sigma_mean())
-
+    errori_deltaN = np.ones(len(delta_P))  # Errori su DeltaN
     
     
-    d = 1  # Spessore noto
-    lambda_ = 1  # Lunghezza d'onda nota
+    
+    d = 3  # Spessore noto, in cm
+    lambda_ =632.8*pow(10,-7)   # Lunghezza d'onda nota,in cm
 
      # Grafico
     grafico_deltaN(P_i, P_f, deltaN, d, lambda_, errori_deltaN, errori_P_i, errori_P_f)
